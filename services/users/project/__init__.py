10381b38259172e39082cee6eda5a11336904bdf
Valentin Bandelier <vmbandelier@gmail.com>
2021-02-16 21:53:42 +0100

    first commit

---
+++ services/users/project/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+
+# instanciate the app
+app = Flask(__name__)
+
+api = Api(app)
+
+# set config
+app.config.from_object('project.config.DevelopmentConfig')
+
+
+class UsersPing(Resource):
+    def get(self):
+        return {
+            'satus': 'success',
+            'message': 'pong!'
+        }
+
+
+api.add_resource(UsersPing, '/users/ping')
