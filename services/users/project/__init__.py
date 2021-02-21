a019d6682fcf240cf8ad065888ab46494566070f
Valentin Bandelier <vmbandelier@gmail.com>
2021-02-21 10:33:37 +0100

    RESTful Routes

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,21 +1,30 @@
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
-
-# instanciate the app
-app = Flask(__name__)
-
-api = Api(app)
-
-# set config
-app.config.from_object('project.config.DevelopmentConfig')
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
-class UsersPing(Resource):
-    def get(self):
-        return {
-            'satus': 'success',
-            'message': 'pong!'
-        }
+# instantiate the db
+db = SQLAlchemy()
 
 
-api.add_resource(UsersPing, '/users/ping')
+def create_app(script_info=None):
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
